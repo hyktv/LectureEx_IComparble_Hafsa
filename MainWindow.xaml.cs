4a1734030dbcbe3dab4fed374c03f92dfcb61a4f
hyktv <hafsa.yktv@gmail.com>
2024-03-18 17:48:33 -0700

    :)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,38 @@ namespace LectureEx_IComparble_Hafsa
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Student> student = new List<Student>();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //1) The IComparable and IComparer interfaces in C# are used for sorting and comparing objects in collections like lists, arrays, and other data structures
+
+            //2) a zero, negative number and a positive number
+
+            //3) you use the Sort method 
+
+            //4) the <T> signifies that these interfaces are generic meaning they can work with any type
+
+            student.Add(new Student("John", "Doe"));
+            student.Add(new Student("Jane", "Smith"));
+            student.Add(new Student("Alice", "Johnson"));
+            student.Add(new Student("Bob", "Brown"));
+            student.Add(new Student("Eve", "Taylor"));
+
+            DisplayStudents();
+
         }
+
+        private void DisplayStudents()
+        {
+            listbox.Items.Clear();
+            foreach (var student in student)
+            {
+                listbox.Items.Add(student.ToString());
+            }
+        }
+
     }
 }
---
+++ Student.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LectureEx_IComparble_Hafsa
+{
+    internal class Student
+    {
+        //feilds
+        string _firstName;
+        string _lastName;
+        double _csiGrade;
+        double _genEdGrade;
+        Random random = new Random();
+        
+        //constructor
+        public Student(string firstName, string lastName)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _csiGrade = random.NextDouble() * 100;
+            _genEdGrade = random.NextDouble() * 100;
+        }
+
+        //properties 
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+        public double CSIGrade
+        {
+            get { return _csiGrade; }
+            set { _csiGrade = value; }
+        }
+
+        public double GenEdGrade
+        {
+            get { return _genEdGrade; }
+            set { _genEdGrade = value; }
+        }
+
+        //method
+        public override string ToString()
+        {
+            return $"Name: {_firstName} {_lastName}, CSI Grade: {_csiGrade}, Gen Ed Grade: {_genEdGrade}";
+        }
+
+    }
+}
